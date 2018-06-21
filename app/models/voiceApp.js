03110b57d231eaa32ca21f1397bdf29ff1d436ea
David Dijkhoff <David@MacBook-Pro-van-David.local>
2018-06-21 15:14:08 -0400

    initial commit

---
+++ app/models/voiceApp.js
@@ -0,0 +1,15 @@
+// load the things we need
+var mongoose = require('mongoose');
+
+// define the schema for our voice application model
+var voiceAppSchema = mongoose.Schema({
+
+    name            : String,
+    secret          : String,
+    scope           : String,
+    owner           : String
+});
+
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('voiceApp', voiceAppSchema);
